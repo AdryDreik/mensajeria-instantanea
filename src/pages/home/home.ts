7b909f7f683d0af2c56940ca4cc967921920e931
Adrian <adry.dabp@gmail.com>
2018-08-29 22:58:16 -0400

    adicionando interfaces

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,21 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { ConversationPage } from '../conversation/conversation';
-
+import { User } from '../../app/interfaces/user';
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
-export class HomePage {
-
+export class HomePage{
+  Users = [];
   constructor(public navCtrl: NavController) {
+    this.Users.push({
+      nick: 'adry',
+      age: 18,
+      email: 'adry.dabp@gmail.com',
+      friend: 'cinthy',
+      uid: '1'
+    });
   }
   gotToConversation () {
     this.navCtrl.push(ConversationPage);
